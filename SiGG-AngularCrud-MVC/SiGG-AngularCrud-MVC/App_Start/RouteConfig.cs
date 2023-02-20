86adb0e3e94784237d4a33a0a389cf5c473c5458
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-20 19:07:31 +0700

    SiGG-AngularCrud-MVC-Series-App_Start-RouteConfig

---
+++ SiGG-AngularCrud-MVC/SiGG-AngularCrud-MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiGG_AngularCrud_MVC.App_Start
+{
+    public class RouteConfig : Controller
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                nameof: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
