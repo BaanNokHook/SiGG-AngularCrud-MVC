7d5c2f192c50a317d9606cdb1613c677fc809025
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-20 20:51:25 +0700

    SiGG-AngularCrud-MVC-Series-Controller-HomeController

--- SiGG-AngularCrud-MVC/SiGG-AngularCrud-MVC/Controllers/HomeController.cs
+++ SiGG-AngularCrud-MVC/SiGG-AngularCrud-MVC/Controllers/HomeController.cs
@@ -1,32 +1,126 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SiGG_AngularCrud_MVC.Models;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 
 namespace SiGG_AngularCrud_MVC.Controllers
 {
-    public class HomeController : Controller
+    public class Homecontroller : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        EmployeeEntities EmpDb = new EmployeeEntities();   
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        // GET: Home
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public JsonResult GetAllEmployee()
         {
-            return View();
+            string message = null;
+            string status = null;
+            List<Employee> emp = new List<Employee>();   
+            try
+            {
+                emp = EmpDb.Employee.ToList();
+                status = "ok";   
+            } 
+            catch (Exception ex)
+            {
+                status = "failed";
+                message = ex.Message.ToString(); 
+            }
+
+            return Json(new { emp, status, message }, JsonRequestBehavior.AllowGet);   
+        }  
+
+        public JsonResult GetEmployeeById(int Id)
+        {
+            string message = null;
+            string status = null;
+            Employee emp = new Employee();
+            try
+            {
+                emp = empDb.Employee.Find(Id);
+                status = "ok";   
+            }
+            catch (Exception ex)
+            {
+                status = "failed";
+                message = ex.Message;  
+            }
+            return Json(new { emp, status, message }, JsonRequestBehavior.AllowGet);   
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public JsonResult AddEmployee(Employee emp)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            string message = null;
+            string status = null;
+            try
+            {
+                EmpDb.Employee.Add(emp);
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Employee record saved sucessfully";
+            }
+            catch (Exception ex)
+            {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json(new { status, message }, JsonRequestBehavior.AllowGet);
+
         }
+
+        public JsonResult DeleteEmployeeById(int Id)
+        {
+            string message = null;
+            string status = null;
+            Employee emp = new Employee();
+            try
+            {
+                emp = EmpDb.Employee.Find(Id);
+                EmpDb.Employee.Remove(emp);
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Employee records deleted successfully";
+            }
+            catch (Exception ex)
+            {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json(new { status, message }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult UpdateEmployee(Employee emp)
+        {
+            string message = null;
+            string status = null;
+            //Employee emp = new Employee();
+            try
+            {
+                Employee obj = EmpDb.Employee.Where(x => x.Emp_Id == emp.Emp_Id).FirstOrDefault();
+                obj.Emp_Name = emp.Emp_Name;
+                obj.Emp_Age = emp.Emp_Age;
+                obj.Emp_City = emp.Emp_City;
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Record Updated Successfully";
+            }
+            catch (Exception ex)
+            {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json(new { status, message }, JsonRequestBehavior.AllowGet);
+
+        }
+
     }
 }
