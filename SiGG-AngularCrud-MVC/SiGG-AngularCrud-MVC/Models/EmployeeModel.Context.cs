0b48baa7eab3e1f49c08b88a3feda6fce5eccca6
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 10:10:11 +0700

    SiGG-AngularCrud-MVC-Series-Models-EmployeeModel.Context

---
+++ SiGG-AngularCrud-MVC/SiGG-AngularCrud-MVC/Models/EmployeeModel.Context.cs
@@ -0,0 +1,24 @@
+﻿using SiGG_AngularCrud_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+namespace SiGG_AngularCrud_MVC.Models
+{
+    public partial class EmployeeEntities : DbContext   
+    {
+        public EmployeeEntities ()
+            : base("name=EmployeeEntities")   
+        {
+        }
+
+        protected override void OnModeCreating(DbModeBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();  
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }   
+    }
+}
