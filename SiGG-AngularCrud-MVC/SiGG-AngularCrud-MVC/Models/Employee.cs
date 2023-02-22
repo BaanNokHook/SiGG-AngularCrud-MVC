00fe915552b850224849c03094e122a5c8ecdab0
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 09:56:03 +0700

    SiGG-AngularCrud-MVC-Series-Models-Employee

---
+++ SiGG-AngularCrud-MVC/SiGG-AngularCrud-MVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace SiGG_AngularCrud_MVC.Models
+{
+    public partial class Employee
+    {
+        public int Emp_Id { get; set; }
+        public string Emp_Name { get; set; }
+        public string Emp_City { get; set; }
+        public Nullable<int> Emp_Age { get; set; }
+    }
+}
